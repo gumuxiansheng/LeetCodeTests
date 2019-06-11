f2af6404227526305dc834db6f4e3f6e7ddd3ec8
Mike Zhu <zhuyuan2706@gmail.com>
2019-06-11 10:37:02 +0800

    Update

--- 22.括号生成.java
+++ 22.括号生成.java
@@ -1,5 +1,6 @@
 import java.util.HashMap;
 import java.util.List;
+import java.util.Set;
 
 /*
  * @lc app=leetcode.cn id=22 lang=java
@@ -18,27 +19,23 @@ class Solution {
             parenthesisList.add("(())");
             return parenthesisList;
         }
-        
-        List<String> iteratedParenthesis = generateParenthesis(n-1);
+
+        List<String> iteratedParenthesis = generateParenthesis(n - 1);
+        Set<String> set = new HashSet<String>();
         for (String parenthesis : iteratedParenthesis) {
             for (int i = 1; i < parenthesis.length(); i++) {
-                // we needn't "()-" and "-()" because every n-1 > 1 will generate "()-" and "-()" so adding "()" between "()" and "-" can reach the same result.
+                // we needn't "()-" and "-()" because every n-1 > 1 will generate "()-" and
+                // "-()" so adding "()" between "()" and "-" can reach the same result.
                 StringBuffer parenthesisBuffer = new StringBuffer(parenthesis);
                 parenthesisBuffer.insert(i, "()");
-                String parenthesisBufferStr = parenthesisBuffer.toString();
-                if (!parenthesisList.contains(parenthesisBufferStr)) {
-                    parenthesisList.add(parenthesisBufferStr);
-                }
+                set.add(parenthesisBuffer.toString());
             }
 
-            String parenthesisNextStr = "(" + parenthesis + ")";
-            if (!parenthesisList.contains(parenthesisNextStr)) {
-                parenthesisList.add(parenthesisNextStr);
-            }
-            
+            set.add("(" + parenthesis + ")");
+
         }
+        parenthesisList = new ArrayList<String>(set);
 
         return parenthesisList;
     }
 }
-
